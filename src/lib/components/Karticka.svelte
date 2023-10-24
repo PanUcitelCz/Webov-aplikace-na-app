e4dad874801c29ae131d3a67677b9065124aa6da
unknown <admin@B9-PII-B44-WZ24.souepl.local>
2023-10-24 13:47:26 +0200

    Další komponenta karticka

---
+++ src/lib/components/Karticka.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    interface $$Props {
+      obrazek: string;
+    }
+  </script>
+  
+  <div class="karticka">
+    <svelte:element this={"div"} class={`okenko}`}>
+      <svelte:element this={"h2"} class={`nadpis}`}>
+        <slot />
+      </svelte:element>
+      <svelte:element this={"img"} src={obrazek} class={`img`} />
+    </svelte:element>
+  </div>
+
+<style lang="stylus">
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Karticka from "$components/Karticka.svelte";
+
+    
+</script>
+
+<Karticka obrazek="https://www.fpe.zcu.cz/cs/media/hero-banner-main/pytle_big.jpeg" >
+    Ahoj
+</Karticka>
